4bf005e52f030a8cc6b418030e50886efbfc3895
tanec <tanec_991@mail.ru>
2025-04-24 20:32:29 +1000

    реализация апи

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,15 @@
+import json
 from datetime import datetime
 import pandas as pd
 from pandas import DataFrame
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+EXCHANGE_RATES_API_KEY = os.getenv("API_KEY")
+BASE_API_URL = "https://api.apilayer.com/exchangerates_data/convert"
 
 def get_taim_greeting():
     """ Фуекция возращает: «Доброе утро» / «Добрый день» / «Добрый вечер» / «Доброй ночи»
@@ -57,15 +65,93 @@ def cards_masc_get(sort_period: DataFrame) -> list[dict]:
 
     card_sort = sort_period[
         [
-            'Номер карты',
+         'Номер карты',
          'Сумма операции',
          'Кэшбэк',
          'Сумма операции с округлением'
         ]
     ]
     for index, value in card_sort.iterrows():
-        print(f"Идекс:{index}, Значение: {value}")
         if value['Сумма операции'] < 0:
             last_digits = str(value['Номер карты']).replace("*","")
             total_spent = value['Сумма операции с округлением']
-            cashback = value['Кэшбэк']
+            cashback = total_spent // 100
+
+            row_dict = {
+                "last_digits": last_digits,
+                "total_spent": total_spent,
+                "cashback":cashback
+            }
+            transactions_cards.append(row_dict)
+    return transactions_cards
+
+def transactions_top(sort_period: DataFrame, top_get):
+    """
+        Функция принимает DataFrame и возращает топ транзакций по сумме платежа
+    """
+
+    title_top_transaction = []
+    sorted_pay = sort_period.sort_values(by='Сумма операции', ascending=False)
+    top_transaction = sorted_pay.head(top_get)
+    top_transaction_sorted = top_transaction[
+        [
+         'Дата платежа',
+         'Сумма операции',
+         'Категория',
+         'Описание'
+        ]
+    ]
+    for index, value in top_transaction_sorted.iterrows():
+        print(value)
+        transaction = {
+          "date": f'{value['Дата платежа']}',
+          "amount": f'{value['Сумма операции']}',
+          "category": f'{value['Категория']}',
+          "description": f'{value['Описание']}'
+        }
+        title_top_transaction.append(transaction)
+    return title_top_transaction
+
+
+def get_currency(path_file_json: str) -> list[dict]:
+    """
+        Функция принимает path_file_json и возращает курс валют
+    """
+    cerence_rates = []
+    with open(path_file_json,'r', encoding='utf-8') as file:
+        data = json.load(file)
+        curences = data['user_currencies']
+        for curence in curences:
+            params = {
+                'amount':1,
+                'from': f'{curence}',
+                'to': 'RUB'
+            }
+            headers = {
+                'apikei': EXCHANGE_RATES_API_KEY,
+            }
+            response = requests.request("GET", BASE_API_URL, headers=headers, data=params)
+            status_code = response.status_code
+            if status_code == 200 :
+                result = response.json()
+                curence_response = result['query']['from']
+                curence_amount = round(result['result'],2)
+                cerence_rates.append(
+                    {
+                            "currency": f'{curence_response}',
+                            "rate": f'{curence_amount}'
+                    }
+                )
+        return  cerence_rates
+
+
+def get_stocks(path_file_json: str) -> list[dict]:
+    """
+        Функция которая принимает path_file_json и возращает cтоимость акций из S&P500.
+    """
+    stocks_rates = []
+    with open(path_file_json, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        stocks = data['user_stocks']
+        for stock in stocks:
+            pass
--- src/views.py
+++ src/views.py
@@ -1,5 +1,9 @@
 from typing import Dict,Any
-from src.utils import get_taim_greeting, get_period_taim, get_path_period, cards_masc_get
+from src.utils import (get_taim_greeting,
+                       get_period_taim, get_path_period,
+                       cards_masc_get, transactions_top,
+                       get_currency,
+                       get_stocks)
 import json
 
 
@@ -8,16 +12,30 @@ def accept_date(data_taim: str) -> Dict[str,Any]:
     """ Функция, принимающая на вход строку с датой и временем в формате YYYY-MM-DD HH:MM:SS
     и возвращающую JSON-ответ
     """
-
-    greeting = get_taim_greeting()
+    # Делайем срез на определенный диапозон
     period_of_days = get_period_taim(data_taim)
-    sort_period = get_path_period('../data/operations.xlsx',period_of_days)
+    sort_period = get_path_period('../data/operations.xlsx', period_of_days)
+
+    # 1. Приветствие
+    greeting = get_taim_greeting()
+
+    # 2. По каждой карте
     user_cards = cards_masc_get(sort_period)
 
+    # 3. Топ пяти транзакций
+    top_transactions = transactions_top(sort_period, 5)
 
+    # 4. Курс валют
+    currency_rates =  get_currency('../data/user_settings.json')
+
+    # 4. Курс валют
+    stock_prices = get_stocks('../data/user_settings.json')
 
     data = {
-        'greeting':greeting
+        'greeting': greeting,
+        'cards': user_cards,
+        'top_transactions': top_transactions,
+        'currency_rates': stock_prices,
     }
 
     json_data = json.dumps(data, ensure_ascii=False, indent=4)
